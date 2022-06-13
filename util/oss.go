10fc31f53c1123c26af1a7cc6abcabc709ed97f6
46160 <nevergiveup1999>
2022-06-13 09:51:46 +0800

    修复bug

--- util/oss.go
+++ util/oss.go
@@ -2,9 +2,11 @@ package util
 
 import (
 	"context"
+	"fmt"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"io"
+	"time"
 )
 
 var client *minio.Client
@@ -38,6 +40,10 @@ func PutVideo(name string, reader io.Reader, size int64) error {
 	return PutObject(name, reader, size, "video/mp4")
 }
 
+func PutJpg(name string, reader io.Reader, size int64) error {
+	return PutObject(name, reader, size, "image/jpeg")
+}
+
 func PutObject(name string, reader io.Reader, size int64, contentType string) error {
 	_, err := client.PutObject(ctx, MINIO_BUCKET, name, reader, size, minio.PutObjectOptions{
 		ContentType: contentType,
@@ -53,15 +59,24 @@ func StatObject(name string) (minio.ObjectInfo, error) {
 	return client.StatObject(ctx, MINIO_BUCKET, name, minio.StatObjectOptions{})
 }
 
-func GetObjectWithSize(name string) (io.Reader, int64, error) {
+func GetUrl(name string) string {
+	presignedURL, err := client.PresignedGetObject(context.Background(), MINIO_BUCKET, name, time.Second*24*60*60, nil)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return "http://" + presignedURL.Host + presignedURL.RequestURI()
+}
+
+func GetObjectWithSize(name string) (io.Reader, int64, time.Time, error) {
 	info, err := StatObject(name)
 	if err != nil {
-		return nil, -1, err
+		return nil, -1, time.Now(), err
 	}
 	object, err := GetObject(name)
 	if err != nil {
-		return nil, -1, err
+		return nil, -1, time.Now(), err
 	}
-
-	return object, info.Size, nil
+	return object, info.Size, info.LastModified, nil
 }
